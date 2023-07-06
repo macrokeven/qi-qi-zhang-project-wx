6d70294886abed0f4c333d561798b4a914da8166
macrokeven <lyhkeven@126.com>
2023-07-06 20:30:55 +0800

    update

--- utils/MyRequest.js
+++ utils/MyRequest.js
@@ -1,67 +1,51 @@
-const GET = 'GET';
-const POST = 'POST';
-const PUT = 'PUT';
-const FORM = 'FORM';
-const DELETE = 'DELETE';
-
 const devURL = 'http://localhost:8001/';
 const devIpURL = 'http://192.168.3.46:8001/';
 const publishURL = getApp().globalData.requestUrl;
-const myUrl = devURL
+const myUrl = devURL;
 
-function noAuthRequest(method, url, data) {
-    return new Promise(function (resolve, reject) {
+function handleRequest(method, url, data, authHeader = false) {
+    return new Promise((resolve, reject) => {
+        const requestUrl = `${myUrl}${url}`;
+        const header = {
+            'content-type': 'application/json',
+        };
+
+        if (authHeader) {
+            header.Authorization = getApp().globalData.userInfo.token;
+        }
+
         wx.request({
-            url: myUrl + url,
+            url: requestUrl,
             method: method,
-            data: method === POST ? JSON.stringify(data) : data,
-            header: {
-                'content-type': 'application/json',
-            },
+            data: method === 'POST' ? JSON.stringify(data) : data,
+            header: header,
             success(res) {
-                if(res.statusCode === 200){
+                if (res.statusCode === 200) {
                     resolve(res.data);
-                }else{
-                    reject(res)
+                } else {
+                    reject(res);
                 }
             },
             fail(err) {
-                //请求失败
-                reject(err)
-            }
-        })
-    }) 
+                reject(err);
+            },
+        });
+    });
 }
 
-function authRequest(method, url, data) {
-    return new Promise(function (resolve, reject) {
-        wx.request({
-            url: myUrl + url,
-            method: method,
-            data: method === POST ? JSON.stringify(data) : data,
-            header: {
-                'content-type': 'application/json',
-                Authorization:getApp().globalData.userInfo.token,
-            },
-            success(res) {
-                if(res.statusCode === 200){
-                    resolve(res.data);
-                }else{
-                    reject(res)
-                }
-            },
-            fail(err) {
-                //请求失败
-                reject(err)
-            }
-        })
-    })
+function noAuthRequest(method, url, data = {}) {
+    return handleRequest(method, url, data);
+}
+
+function authRequest(method, url, data = {}) {
+    return handleRequest(method, url, data, true);
 }
 
 const API = {
-    noAuthRequest: (method, url, data) => noAuthRequest(method, url, data),
-    authRequest:(method, url, data) => authRequest(method, url, data),
+    noAuthRequest,
+    authRequest,
 };
+
 module.exports = {
-    API: API
-}
+    API: API,
+};
