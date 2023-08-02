67deccc60be4ad33e3959d6f490da3e76eeed311
macrokeven <lyhkeven@126.com>
2023-08-03 02:07:55 +0800

    update

--- pages/real-id-verification/real-id-verification.js
+++ pages/real-id-verification/real-id-verification.js
@@ -1,13 +1,49 @@
 // pages/real-id-verification/real-id-verification.js
+import Message from 'tdesign-miniprogram/message/index';
+
 Page({
 
     /**
      * 页面的初始数据
      */
-    data: {
-
+    data: {},
+    uploadPhoto() {
+        wx.chooseMedia({
+            count: 1,
+            mediaType: ['image'],
+            sourceType: ['album', 'camera'],
+            camera: 'back',
+            success(res) {
+                if (res.tempFiles.size > 4 * 1000 * 1000) {
+                    Message.warning({
+                        context: this,
+                        offset: [20, 32],
+                        marquee: {loop: 0},
+                        duration: 2000,
+                        content: '文件大小超过4MB！'
+                    })
+                } else {
+                    const uploadTask = wx.uploadFile({
+                        header: {
+                            "Authorization": getApp().globalData.userInfo.token
+                        },
+                        url: 'http://localhost:8001/FileUpload/UploadPicture', //仅为示例，非真实的接口地址
+                        filePath: res.tempFiles[0].tempFilePath,
+                        name: 'multipartFile',
+                        formData: {
+                            'user': 'test'
+                        },
+                        success(res) {
+                            console.log(res);
+                        },
+                        fail(err) {
+                            console.log(err);
+                        }
+                    })
+                }
+            }
+        })
     },
-
     /**
      * 生命周期函数--监听页面加载
      */
